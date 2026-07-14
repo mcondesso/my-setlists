0392751cec35c43da94b0c64b5c10e28a4bced97
mcondesso <mcondesso@mail.com>
2026-07-14 14:47:32 +0200

    Add uvicorn app reload as configurable env variable

--- main.py
+++ main.py
@@ -2,5 +2,8 @@
 
 import uvicorn
 
+from src.core.config import settings
+
+
 if __name__ == "__main__":
-    uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=settings.UVICORN_RELOAD)
