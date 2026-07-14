f834fd5ba60da4bcf6bcbc92f5ccdda809e48159
mcondesso <mcondesso@mail.com>
2026-07-14 16:29:30 +0200

    Read APP_PORT from environment variable

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ from src.core.config import settings
 
 
 if __name__ == "__main__":
-    uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=settings.UVICORN_RELOAD)
+    uvicorn.run(
+        "src.app:app",
+        host="0.0.0.0",
+        port=settings.APP_PORT,
+        reload=settings.UVICORN_RELOAD,
+    )
